773ebe8929d9922de3891235b2a07b9b40ff4e6c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-05-16 23:53:51 -0500

    Done.

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -5,26 +5,27 @@ class Boat < ActiveRecord::Base
 
   def self.first_five
     # all.limit(5)
+    limit(5).to_a
   end
 
   def self.dinghy
-    # where("length < 20")
+    where("length < 20")
   end
 
   def self.ship
-    # where("length >= 20")
+    where("length >= 20")
   end
 
   def self.last_three_alphabetically
-    # all.order(name: :desc).limit(3)
+    all.order(name: :desc).limit(3)
   end
 
   def self.without_a_captain
-    # where(captain_id: nil)
+    where(captain_id: nil)
   end
 
   def self.sailboats
-    # includes(:classifications).where(classifications: { name: 'Sailboat' })
+    includes(:classifications).where(classifications: { name: 'Sailboat' })
   end
 
   def self.with_three_classifications
@@ -32,14 +33,14 @@ class Boat < ActiveRecord::Base
     # regularly. Just know that we can get this out of the database in
     # milliseconds whereas it would take whole seconds for Ruby to do the same.
     #
-    # joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
+     joins(:classifications).group("boats.id").having("COUNT(*) = 3").select("boats.*")
   end
 
   def self.non_sailboats
-    # where("id NOT IN (?)", self.sailboats.pluck(:id))
+    where("id NOT IN (?)", self.sailboats.pluck(:id))
   end
 
   def self.longest
-    # order('length DESC').first
+    order('length DESC').first
   end
 end
